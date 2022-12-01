15f871d839718cbbf41a5ed13088c522cfcf8b66
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-12-01 17:21:27 -0300

    boilerplate code and basic template created

---
+++ src/ClassState.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ClassState extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Eliminar ClassState</h2>
+
+                <p>Pro favor, escribe el codigo de seguridad</p>
+
+                <input placeholder="codigo de seguridad"/>
+                <button>Comprobar</button>
+            </div>
+        )
+    }
+}
+
+export {ClassState};
