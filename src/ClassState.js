c56dfd8c77c6cd4a29d90eb32d93ac40a2b689e7
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-12-02 09:31:41 -0300

    defined state and class state for error handling

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,15 +1,33 @@
 import React from "react";
 
 class ClassState extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          error:false,
+        };
+    }
     render() {
         return(
             <div>
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar {this.props.name}</h2>
 
                 <p>Pro favor, escribe el codigo de seguridad</p>
 
+                {this.state.error && (
+                    <p>Error el codigo es incorrecto</p>
+                )}
+
                 <input placeholder="codigo de seguridad"/>
-                <button>Comprobar</button>
+                <button
+                    onClick={() =>
+                        this.setState(prevState => (
+                            {error: !prevState.error})
+                        )}
+                >
+                    Comprobar
+                </button>
             </div>
         )
     }
--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-function UseState() {
+function UseState({name}) {
+    const [error,serError] = React.useState(false);
     return(
         <div>
-            <h2>Eliminar UseState</h2>
+            <h2>Eliminar {name}</h2>
 
             <p>Pro favor, escribe el codigo de seguridad</p>
 
+            {error && (
+                <p>Error el codigo es incorrecto</p>
+            )}
+
             <input placeholder="codigo de seguridad"/>
-            <button>Comprobar</button>
+            <button
+                onClick={() => setError(!error)}
+            >
+                Comprobar
+            </button>
         </div>
     )
 }
